fcd44c79340c93e7797000a7cac4c40a2fd7ec5b
shi-jialei <56877845+shi-jialei@users.noreply.github.com>
2026-07-26 20:58:28 +0000

    Deploying to gh-pages from @ shi-jialei/shi-jialei.github.io@793378940c6fa57b39bd68499b60b565f802828d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -487,8 +487,8 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-physical-human-robot-interaction-and-haptics",
-          title: 'Physical human–robot interaction and haptics',
-          description: "Active regulation and perception of robot-environment interaction and haptics",
+          title: 'Physical Human–Robot Interaction and Haptics',
+          description: "Active regulation and perception of robot–environment interaction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{
